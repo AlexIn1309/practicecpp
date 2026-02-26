f4f2e8bea111b82cd03fe2c0aa9e365ae3dda064
AlexIn1309 <moran.graciano.cesar@gmail.com>
2026-02-26 13:52:30 -0600

    Realiza resta de dos numeros

---
+++ resta.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+  int num1 = 7;
+  int num2 = 9;
+  std::cout << "La resta de " << num1 << " - " << num2 << " = " << num1 - num2;
+  return 0;
+}
---
+++ suma.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main (int argc, char *argv[]) {
+  int num1 = 5;
+  int num2 = 20;
+  std::cout << "La suma de " << num1 << " + " << num2 << " = " << num1 + num2;
+  return 0;
+}
