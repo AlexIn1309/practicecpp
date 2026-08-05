3489c6ab0e35d7b5a611aa1d19755a77a571161c
alejandro <pickledgob@icloud.com>
2026-08-05 11:44:18 -0600

    Creacion de reto Romanos a Enteros

---
+++ RomanToInteger/RomanToInteger.c
@@ -0,0 +1,23 @@
+/* ROMAN TO INTEGER
+ *	Recibe una cadena de string s,
+ *	Con un valor Romano y este valor
+ *	se debe convertir  numero entero */
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+		a
+    }
+};
+
+int main(){
+	Solution sol;
+	cout << "Primera Prueba (III): "<<sol.romanToInt('III')<<endl;
+	cout << "Primera Prueba (LVIII): "<<sol.romanToInt('LVIII')<<endl;
+	cout << "Primera Prueba (MCMXCIV): "<<sol.romanToInt('MCMXCIV')<<endl;
+	return 0;
+}
