36b1d17865031ccc6932f1a0eda2ac2d3954025e
alejandro <pickledgob@icloud.com>
2026-08-06 10:18:18 -0600

    Numero romanos a enteros

--- RomanToInteger/RomanToInteger.c
+++ RomanToInteger/RomanToInteger.c
@@ -4,20 +4,74 @@
  *	se debe convertir  numero entero */
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Solution {
 public:
     int romanToInt(string s) {
-		a
+		int tamano = s.size();
+		int suma = 0;
+		char ultimoCaracter = 'a';
+		for (char letra: s) {
+			if (letra == 'M') {
+				suma += 1000;
+				if (ultimoCaracter == 'C') {
+					suma -= 200;
+				}
+				cout << " Sumamos: "<<suma<<endl;
+			}
+			if (letra == 'D') {
+				suma += 500;
+				if (ultimoCaracter == 'C') {
+					suma -= 200;
+				}
+				cout << " Sumamos: "<<suma<<endl;
+			}
+			if (letra == 'C') {
+				suma += 100;
+				if (ultimoCaracter == 'X') {
+					suma -= 20;
+				}
+				cout << " Sumamos: "<<suma<<endl;
+			}
+			if (letra == 'L') {
+				suma += 50;
+				if (ultimoCaracter == 'X') {
+					suma -= 20;
+				}
+				cout << " Sumamos: "<<suma<<endl;
+			}
+			if (letra == 'X') {
+				suma += 10;
+				if (ultimoCaracter == 'I') {
+					suma -= 2;
+				}
+				cout << " Sumamos: "<<suma<<endl;
+			}
+			if (letra == 'I') {
+				suma += 1;
+				cout << " Sumamos: "<<suma<<endl;
+			}
+			if (letra == 'V') {
+				suma += 5;
+				if (ultimoCaracter == 'I') {
+					suma -= 2;
+				}
+				cout << " Sumamos: "<<suma<<endl;
+			}
+			cout << " Ultima letra: "<<ultimoCaracter<<endl;
+			ultimoCaracter = letra;
+		}
+		return suma;
     }
 };
 
 int main(){
 	Solution sol;
-	cout << "Primera Prueba (III): "<<sol.romanToInt('III')<<endl;
-	cout << "Primera Prueba (LVIII): "<<sol.romanToInt('LVIII')<<endl;
-	cout << "Primera Prueba (MCMXCIV): "<<sol.romanToInt('MCMXCIV')<<endl;
+	cout << "Primera Prueba (III): "<<endl<<sol.romanToInt("III")<<endl<<" Esperado: 3"<<endl;
+	cout << "Primera Prueba (LVIII): "<<endl<<sol.romanToInt("LVIII")<<endl<<" Esperado: 58"<<endl;
+	cout << "Primera Prueba (MCMXCIV): "<<endl<<sol.romanToInt("MCMXCIV")<<endl<<" Esperado: 1994"<<endl;
 	return 0;
 }
