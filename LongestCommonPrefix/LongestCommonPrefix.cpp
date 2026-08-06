e4c390b6704a735cabc07635ad62221e0e8d8fda
alejandro <pickledgob@icloud.com>
2026-08-06 12:43:08 -0600

    El prefijo comun mas pequeno

--- LongestCommonPrefix/LongestCommonPrefix.cpp
+++ LongestCommonPrefix/LongestCommonPrefix.cpp
@@ -11,15 +11,53 @@
  * Output: ""*/
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        
+    string longestCommonPrefix(const vector<string>& strs) {
+        string prefijoComun = "";
+        bool bandera = true;
+        int contador = 0;
+
+        if (strs.empty()) return "";
+
+        while (bandera) {
+            // Guardar la letra de referencia
+            char primera_letra = strs[0][contador];
+			cout<<endl<<primera_letra<<endl;
+
+            // Evalúa si TODAS las palabras coinciden en el índice 'contador'
+            bool todas_iguales = std::all_of(strs.begin(), strs.end(), [primera_letra, contador](const string& str) {
+                return !str.empty() && str[contador] == primera_letra;
+            });
+
+            bandera = todas_iguales;
+			if (!bandera) {
+				break;
+			}
+			prefijoComun.push_back(primera_letra);
+            contador++;
+        }
+
+        return prefijoComun; // Agregado el return que faltaba
     }
 };
 
 int main(){
-	Solution sol;
+    Solution sol;
 
+    cout << "Primera Prueba (['flower', 'flow', 'flight']): " << endl 
+         << sol.longestCommonPrefix({"flower", "flow", "flight"}) << endl 
+         << " Esperado: fl" << endl;
+
+    cout << "Segunda Prueba (['dog', 'racecar', 'car']): " << endl 
+         << sol.longestCommonPrefix({"dog", "racecar", "car"}) << endl 
+         << " Esperado: " << endl;
+
+    return 0;
 }
